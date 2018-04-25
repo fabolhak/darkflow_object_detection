5ed55ddc0332f832c72b111017ad8206ab79e0c9
FaHa <dev-faha@t-online.de>
2018-04-25 09:24:59 +0200

    use of parameters and comments

--- scripts/detection_node.py
+++ scripts/detection_node.py
@@ -13,27 +13,36 @@ from darkflow.net.build import TFNet
 # image detection class
 class image_detection:
 
-  def __init__(self):
-    options = {"model": "../darkflow/cfg/yolo.cfg", "load": "../nets/yolo_50.weights", "threshold": 0.5922, "batch": 1, "gpu": 0.9, "summary": None, "config": "../darkflow/cfg/"}
+  def __init__(self, model_file, weights_file, threshold, config_folder, gpu_usage):
+
+    # initialize darkflow
+    options = {"model": model_file, "load": weights_file, "threshold": threshold,
+               "batch": 1, "gpu": gpu_usage, "summary": None, "config": config_folder}
     self.model = TFNet(options)
+
+    # create ros publisher and subscriber
     self.bridge = CvBridge()
-    self.image_pub = rospy.Publisher("/left/detected",Image,queue_size=1)
-    self.obj_pub = rospy.Publisher("/left/objects", Objects, queue_size=1)
-    self.image_sub = rospy.Subscriber("/left/image_rect_color", Image, self.callback, queue_size=1)
+    self.image_pub = rospy.Publisher("detected_img",Image,queue_size=1)
+    self.obj_pub = rospy.Publisher("detected_obj", Objects, queue_size=1)
+    self.image_sub = rospy.Subscriber("input_img", Image, self.callback, queue_size=1)
 
   def callback(self,data):
+
+    # convert message data to image
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
 
+    # run the neural network and get outputs
     result = self.model.return_predict(cv_image)
 
+    # create new objects message
     pub_msg = Objects()
     pub_msg.header.stamp = data.header.stamp
     pub_msg.header.frame_id = data.header.frame_id
 
-
+    # save results of neural network in objects message
     for res in result:
       if 'car' in res['label']:
         poly = Polygon()
@@ -49,13 +58,20 @@ class image_detection:
         pub_msg.obj.append(poly)
         pub_msg.confidence.append(float(res['confidence']))
 
+    # publish outputs message
     self.obj_pub.publish(pub_msg)
 
+    # if someone subscribed to debug image -> publish it
     if 0 < self.image_pub.get_num_connections():
+
+      # draw rectangles in image
       for res in result:
         if 'car' in res['label']:
           cv2.rectangle(cv_image, (res['topleft']['x'], res['topleft']['y']),
-                        (res['bottomright']['x'], res['bottomright']['y']), (0, 255, 0), 3)
+                                  (res['bottomright']['x'], res['bottomright']['y']),
+                                  (0, 255, 0), 3)
+
+      # convert to image message and publish
       try:
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
       except CvBridgeError as e:
@@ -65,11 +81,19 @@ class image_detection:
 # ros node
 def node():
     rospy.init_node('detection_node')
-    image_detection()
+
+    # get parameters
+    model_file =    rospy.get_param('~model_file')
+    weights_file =  rospy.get_param('~weights_file')
+    threshold =     rospy.get_param('~threshold')
+    config_folder = rospy.get_param('~config_folder')
+    gpu_usage =     rospy.get_param('~gpu_usage')
+
+    # initialize image detection class
+    image_detection(model_file, weights_file, threshold, config_folder, gpu_usage)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-
 
 if __name__ == "__main__":
     node()
